d10d995c78d612b964992a29dd07792b74341495
Brett <brett.case97@gmail.com>
2020-01-18 17:25:06 -0800

    Adding inital styling for page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function init() {
     var stage = new createjs.Stage("demoCanvas");
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
+
+    let gameContainer = new createjs.Shape() 
+    gameContainer.graphics.beginFill("#FFFFFF").drawRect(40, 40, 720, 720);
+    
+    let nextColorContainer = new createjs.Shape() 
+    nextColorContainer.graphics.beginFill("#FFFFFF").drawRect(800, 40, 215, 360);
+
+    let scoreContainer = new createjs.Shape() 
+    scoreContainer.graphics.beginFill("#FFFFFF").drawRect(800, 440, 215, 320);
+    
+    stage.addChild(gameContainer, nextColorContainer, scoreContainer);
     stage.update();
   }
 
