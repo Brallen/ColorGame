49ff2bbbfa61b77b6a11983fa711867fd8866fd9
Brett <brett.case97@gmail.com>
2020-01-19 16:31:46 -0800

    Converting canvas to just be play area of game

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 function init() {
     var stage = new createjs.Stage("demoCanvas");
 
-    let gameContainer = new createjs.Shape() 
-    gameContainer.graphics.beginFill("#FFFFFF").drawRect(40, 40, 720, 720);
+    let circle = new createjs.Shape() 
+    circle.graphics.beginFill("#43561B").drawCircle(355, 355, 50);
     
-    let nextColorContainer = new createjs.Shape() 
-    nextColorContainer.graphics.beginFill("#FFFFFF").drawRect(800, 40, 215, 360);
-
-    let scoreContainer = new createjs.Shape() 
-    scoreContainer.graphics.beginFill("#FFFFFF").drawRect(800, 440, 215, 320);
-    
-    stage.addChild(gameContainer, nextColorContainer, scoreContainer);
+    stage.addChild(circle);
     stage.update();
   }
 
