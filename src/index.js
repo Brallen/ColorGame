6100b1e4827da66890ffe705916ce784929ba9ed
manason <clanerp@gmail.com>
2020-01-20 19:06:54 -0800

    Game class has addShape() function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-function init() {
-    var stage = new createjs.Stage("demoCanvas");
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
-    stage.update();
-  }
+var game = new Game();
+game.addShape("DeepSkyBlue", [
+  [0, 0],
+  [100, 100],
+  [100, 0],
+  [0, 0],
+]);
 
-window.onload = init;
+game.addShape("Green", [
+  [0, 0],
+  [0, 100],
+  [100, 100],
+  [0, 0]
+]);
+
+game.render();
+
+// window.onload = game.render();
